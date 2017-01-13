9d2506e59f7a66b1154df87aa0e01d70e921b681
dpausp <dpausp@users.noreply.github.com>
2017-01-13 16:41:00 +0000

    add option to select a channel in the default builder

--- default.nix
+++ default.nix
@@ -1,5 +1,13 @@
-{pkg, nixpkgs ? <nixpkgs>}:
+{pkg, nixpkgs ? null, channel ? null }:
 let 
-  pkgs = import nixpkgs {};
+  _nixpkgs = if channel != null then
+    "${builtins.getEnv "HOME"}/.nix-defexpr/channels_root/${channel}/nixpkgs"
+  else
+    if nixpkgs == null then
+      <nixpkgs>
+    else
+      nixpkgs
+  ;
+  pkgs = import _nixpkgs {};
   deps = with pkgs; callPackage pkg {};
 in deps
